46e1b9a9c64540aa48d5b4d489fa1c2e63e67960
MongMX <mongmx@gmail.com>
2018-03-23 00:13:18 +0700

    Add client for user service

---
+++ app/service-test/user-cli/user-cli.go
@@ -0,0 +1,140 @@
+package main
+
+/*import (
+	"log"
+	"os"
+
+	"github.com/micro/cli"
+	"github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	pb "github.com/mongmx/mymicro/app/user-service/proto/user"
+	"golang.org/x/net/context"
+)
+
+func main() {
+
+	cmd.Init()
+
+	// Create new greeter client
+	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
+
+	// Define our flags
+	service := micro.NewService(
+		micro.Flags(
+			cli.StringFlag{
+				Name:  "name",
+				Usage: "You full name",
+			},
+			cli.StringFlag{
+				Name:  "email",
+				Usage: "Your email",
+			},
+			cli.StringFlag{
+				Name:  "password",
+				Usage: "Your password",
+			},
+			cli.StringFlag{
+				Name:  "company",
+				Usage: "Your company",
+			},
+		),
+	)
+
+	// Start as service
+	service.Init(
+
+		micro.Action(func(c *cli.Context) {
+
+			name := c.String("name")
+			email := c.String("email")
+			password := c.String("password")
+			company := c.String("company")
+
+			// Call our user service
+			r, err := client.Create(context.TODO(), &pb.User{
+				Name:     name,
+				Email:    email,
+				Password: password,
+				Company:  company,
+			})
+			if err != nil {
+				log.Fatalf("Could not create: %v", err)
+			}
+			log.Printf("Created: %s", r.User.Id)
+
+			getAll, err := client.GetAll(context.Background(), &pb.Request{})
+			if err != nil {
+				log.Fatalf("Could not list users: %v", err)
+			}
+			for _, v := range getAll.Users {
+				log.Println(v)
+			}
+
+			os.Exit(0)
+		}),
+	)
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		log.Println(err)
+	}
+}*/
+
+import (
+	"log"
+	"os"
+
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/cmd"
+	pb "github.com/mongmx/mymicro/app/user-service/proto/user"
+	"golang.org/x/net/context"
+)
+
+func main() {
+
+	cmd.Init()
+
+	// Create new greeter client
+	client := pb.NewUserServiceClient("go.micro.srv.user", microclient.DefaultClient)
+
+	name := "Ewan Valentine"
+	email := "ewan.valentine89@gmail.com"
+	password := "test123"
+	company := "BBC"
+
+	log.Println(name, email, password)
+
+	r, err := client.Create(context.TODO(), &pb.User{
+		Name:     name,
+		Email:    email,
+		Password: password,
+		Company:  company,
+	})
+	if err != nil {
+		log.Fatalf("Could not create: %v", err)
+	}
+	log.Printf("Created: %s", r.User.Id)
+
+	getAll, err := client.GetAll(context.Background(), &pb.Request{})
+	if err != nil {
+		log.Fatalf("Could not list users: %v", err)
+	}
+	for _, v := range getAll.Users {
+		log.Println(v)
+	}
+
+	authResponse, err := client.Auth(context.TODO(), &pb.User{
+		Email:    email,
+		Password: password,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not authenticate user: %s error: %v\n", email, err)
+	}
+
+	log.Printf("Your access token is: %s \n", authResponse.Token)
+
+	// let's just exit because
+	os.Exit(0)
+}
