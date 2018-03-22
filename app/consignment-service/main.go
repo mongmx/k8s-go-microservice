aad9ef7464762036364c40ccb6c45a877aa32970
MongMX <mongmx@gmail.com>
2018-03-22 19:03:04 +0700

    Add datastore to consignment service

--- app/consignment-service/main.go
+++ app/consignment-service/main.go
@@ -1,80 +1,53 @@
 package main
 
 import (
-
-	// Import the generated protobuf code
 	"fmt"
+	"log"
+	"os"
 
 	micro "github.com/micro/go-micro"
+
+	// Import the generated protobuf code
 	pb "github.com/mongmx/mymicro/app/consignment-service/proto/consignment"
-	"golang.org/x/net/context"
+	vesselProto "github.com/mongmx/mymicro/app/vessel-service/proto/vessel"
 )
 
-// IRepository interface
-type IRepository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-// Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type Repository struct {
-	consignments []*pb.Consignment
-}
-
-// Create function
-func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-// GetAll function
-func (repo *Repository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type service struct {
-	repo IRepository
-}
-
-// CreateConsignment - we created just one method on our service,
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the `Response` message we created in our
-	// protobuf definition.
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultHost = "localhost:27017"
+)
 
 func main() {
 
-	repo := &Repository{}
+	// Database host from the environment variables
+	host := os.Getenv("DB_HOST")
 
+	if host == "" {
+		host = defaultHost
+	}
+
+	session, err := CreateSession(host)
+
+	// Mgo creates a 'master' session, we need to end that session
+	// before the main function closes.
+	defer session.Close()
+
+	if err != nil {
+
+		// We're wrapping the error returned from our CreateSession
+		// here to add some context to the error.
+		log.Panicf("Could not connect to datastore with host %s - %v", host, err)
+	}
+
+	// repo := &Repository{}
+
+	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
 		// This name must match the package name given in your protobuf definition
 		micro.Name("go.micro.srv.consignment"),
 		micro.Version("latest"),
 	)
+
+	vesselClient := vesselProto.NewVesselServiceClient("go.micro.srv.vessel", srv.Client())
 
 	// Init will parse the command line flags.
 	srv.Init()
@@ -82,7 +55,7 @@ func main() {
 	// Register our service with the gRPC server, this will tie our
 	// implementation into the auto-generated interface code for our
 	// protobuf definition.
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
